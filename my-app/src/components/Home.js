ec6ce2dcb3286af0aac42af5d3c3c793e5018472
Nicholas Chikuji <chiku524@icloud.com>
2022-06-26 14:27:59 -0400

    added zoom in functionality for about page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -4,27 +4,46 @@ import CityWalk from '../images/City_walk.png';
 import Gameroom from '../images/Gameroom.png';
 import Skatepark from '../images/Skatepark.png';
 import Nav from '../components/Nav';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import CloseIcon from '@mui/icons-material/Close';
 import '../css/home.css';
 
 const Home = () => {
     const [background, setBackground] = useState([CityWalk]);
     const [images] = useState([CityWalk, Gameroom, Skatepark]);
     const [bgClass, setBgClass] = useState('citywalk');
+    const [clicked, setClicked] = useState(false);
+    const [aboutClass, setAboutClass] = useState('about');
+    const [fullClass, setFullClass] = useState('full');
+
+    const onClickAbout = () => {
+        if(bgClass=='citywalk'){setBgClass(`${bgClass} zoomIn-0`); setAboutClass('about w-0 expand');};
+        if(bgClass=='skatepark'){setBgClass(`${bgClass} zoomIn-1`); setAboutClass('about w-1 expand');};
+        if(bgClass=='gameroom'){setBgClass(`${bgClass} zoomIn-2`); setAboutClass('about w-2 expand');};
+    }
+    const onClickClose = () => {
+        if(bgClass=='citywalk zoomIn-0'){setBgClass(`${bgClass}`)}
+    }
 
     const bgSwitcher = () => {
         if(background==images[0]){
             setBackground(images[1]);
             setBgClass('gameroom');
+            setAboutClass('about w-2');
         }
         if(background==images[1]){
             setBackground(images[2]);
             setBgClass('skatepark');
+            setAboutClass('about w-1');
         }
         if(background==images[2]){
             setBackground(images[0]);
             setBgClass('citywalk');
+            setAboutClass('about w-0');
         }
     }
+
+
 
     return (
       <div className="home">
@@ -33,13 +52,44 @@ const Home = () => {
         <div className={bgClass} />
         <div className={bgClass} />
         <div className={bgClass} />
-        <Nav />
+        <div className='navbar'>
+            <Link to='/home/' className='home-btn'>
+                <HomeRoundedIcon className='home-icon'/>
+            </Link>
+            <Link to='/about/' className='item' onClick={onClickAbout}>
+                <h4>ABOUT</h4>
+            </Link>
+            <Link to='/roadmap/' className='item'>
+                <h4>ROADMAP</h4>
+            </Link>
+            <Link to='/savology/' className='item'>
+                <h4>SAVOLOGY</h4>
+            </Link>
+            <Link to='/store/' className='item'>
+                <h4>STORE</h4>
+            </Link>
+            <Link to='/about/' className='home-btn-row'>
+                <div className='home-trio'>
+                    <HomeRoundedIcon className='home-icon'/>
+                    <HomeRoundedIcon className='home-icon'/>
+                    <HomeRoundedIcon className='home-icon'/>
+                </div>
+            </Link>
+            <Link to='/about/' className='home-btn'>
+                <HomeRoundedIcon className='home-icon'/>
+            </Link>
+        </div>
+        <div className={aboutClass}>
+            <div className='exit'>
+                <CloseIcon onClick={onClickClose}/>
+            </div>
+        </div>
         <div className='transition-btn' onClick={bgSwitcher}>
             <h3>CLICK HERE</h3>
         </div>
       </div>
     );
-  }
+}
   
   export default Home;
   
--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,37 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import CloseIcon from '@mui/icons-material/Close';
 import '../css/nav.css';
 
 const Nav = () => {
+    const [clicked, setClicked] = useState(false);
+    const [aboutClass, setAboutClass] = useState('about');
+    const [fullClass, setFullClass] = useState('full');
+
+    const onClickAbout = () => {
+        if(clicked){setClicked(false); setAboutClass('about'); setFullClass('full')};
+        if(!clicked){setClicked(true); setAboutClass('about zoom'); setFullClass('full zoomed')};
+    }
+
+    console.log(clicked);
 
     return (
-        <div className='navbar'>
-            <Link to='/home/' className='home-btn'>
-                <HomeRoundedIcon className='home-icon'/>
-            </Link>
-            <Link to='/about/' className='item'>
-                <h4>ABOUT</h4>
-            </Link>
-            <Link to='/roadmap/' className='item'>
-                <h4>ROADMAP</h4>
-            </Link>
-            <Link to='/savology/' className='item'>
-                <h4>SAVOLOGY</h4>
-            </Link>
-            <Link to='/store/' className='item'>
-                <h4>STORE</h4>
-            </Link>
-            <Link to='/about/' className='home-btn-row'>
-                <div className='home-trio'>
+        <div className={fullClass}>
+            <div className='navbar'>
+                <Link to='/home/' className='home-btn'>
                     <HomeRoundedIcon className='home-icon'/>
+                </Link>
+                <Link to='/about/' className='item' onClick={onClickAbout}>
+                    <h4>ABOUT</h4>
+                </Link>
+                <Link to='/roadmap/' className='item'>
+                    <h4>ROADMAP</h4>
+                </Link>
+                <Link to='/savology/' className='item'>
+                    <h4>SAVOLOGY</h4>
+                </Link>
+                <Link to='/store/' className='item'>
+                    <h4>STORE</h4>
+                </Link>
+                <Link to='/about/' className='home-btn-row'>
+                    <div className='home-trio'>
+                        <HomeRoundedIcon className='home-icon'/>
+                        <HomeRoundedIcon className='home-icon'/>
+                        <HomeRoundedIcon className='home-icon'/>
+                    </div>
+                </Link>
+                <Link to='/about/' className='home-btn'>
                     <HomeRoundedIcon className='home-icon'/>
-                    <HomeRoundedIcon className='home-icon'/>
+                </Link>
+            </div>
+            <div className={aboutClass}>
+                <div className='exit'>
+                    <CloseIcon onClick={onClickAbout}/>
                 </div>
-            </Link>
-            <Link to='/about/' className='home-btn'>
-                <HomeRoundedIcon className='home-icon'/>
-            </Link>
+            </div>
         </div>
     )
 }
